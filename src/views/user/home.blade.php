dc9d0651957a842bd2a38da7785b093272c15e20
Azri Jamil <azri@nematix.com>
2013-11-25 18:27:28 +0800

    Refactor home view

--- src/views/user/home.blade.php
+++ src/views/user/home.blade.php
@@ -1,8 +1,20 @@
-@extends('layouts.user')
+@extends('admin::layouts.user')
 
-@section('container')
-<div class="row">
+@section('base')
+<div id="wrap">
+<div class="container">
+    <div class="row padded">
+        @if( isset($status) )
+        <div class="alert alert-{{ $status['type'] }}">
+            {{ $status['message'] }}
+        </div>
+        @endif
+    </div>
 
+    <div class="row">
+
+    </div>
+</div>
 </div>
 @stop
 
