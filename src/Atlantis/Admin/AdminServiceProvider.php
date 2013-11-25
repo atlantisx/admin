7ce120af9821bde5481a4346d8560eb4eae56c10
Azri Jamil <azri@nematix.com>
2013-11-25 18:28:09 +0800

    Code cleanup

--- src/Atlantis/Admin/AdminServiceProvider.php
+++ src/Atlantis/Admin/AdminServiceProvider.php
@@ -1,4 +1,23 @@
 <?php namespace Atlantis\Admin;
+/**
+ * Part of the Atlantis package.
+ *
+ * NOTICE OF LICENSE
+ *
+ * Licensed under the 3-clause BSD License.
+ *
+ * This source file is subject to the 3-clause BSD License that is
+ * bundled with this package in the LICENSE file.  It is also available at
+ * the following URL: http://www.opensource.org/licenses/BSD-3-Clause
+ *
+ * @package    Atlantis
+ * @version    1.0.0
+ * @author     Nematix LLC
+ * @license    BSD License (3-clause)
+ * @copyright  (c) 1997 - 2013, Nematix LLC
+ * @link       http://nematix.com
+ */
+
 
 use Atlantis\Admin\Config\Factory as ConfigFactory;
 use Atlantis\Admin\Fields\Factory as FieldFactory;
