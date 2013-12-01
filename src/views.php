90e75bb012543076767e72a16380adf8ea359476
Azri Jamil <azri@nematix.com>
2013-12-01 15:51:27 +0800

    Refactor Atlantis view composer

--- src/views.php
+++ src/views.php
@@ -3,75 +3,9 @@
 use Atlantis\Admin\ModelHelper;
 use Atlantis\Admin\Fields\Field;
 
-//View Composers
 
-//admin index view
-View::composer('admin::admin.admin', function($view)
-{
-	//get a model instance that we'll use for constructing stuff
-	$config = App::make('itemconfig');
-	$fieldFactory = App::make('admin_field_factory');
-	$columnFactory = App::make('admin_column_factory');
-	$actionFactory = App::make('admin_action_factory');
-	$dataTable = App::make('admin_datatable');
-	$model = $config->getDataModel();
-	$baseUrl = URL::route('admin_dashboard');
-	$route = parse_url($baseUrl);
-
-	//add the view fields
-	$view->config = $config;
-	$view->dataTable = $dataTable;
-	$view->primaryKey = $model->getKeyName();
-	$view->editFields = $fieldFactory->getEditFields();
-	$view->arrayFields = $fieldFactory->getEditFieldsArrays();
-	$view->dataModel = $fieldFactory->getDataModel();
-	$view->columnModel = $columnFactory->getColumnOptions();
-	$view->actions = $actionFactory->getActionsOptions();
-	$view->globalActions = $actionFactory->getGlobalActionsOptions();
-	$view->actionPermissions = $actionFactory->getActionPermissions();
-	$view->filters = $fieldFactory->getFiltersArrays();
-	$view->rows = $dataTable->getRows(App::make('db'), $view->filters);
-	$view->formWidth = $config->getOption('form_width');
-	$view->baseUrl = $baseUrl;
-	$view->assetUrl = URL::to('packages/atlantis/admin/');
-	$view->route = $route['path'].'/';
-	$view->itemId = isset($view->itemId) ? $view->itemId : null;
-});
-
-
-//admin settings view
-View::composer('admin::admin.settings', function($view)
-{
-	$config = App::make('itemconfig');
-	$fieldFactory = App::make('admin_field_factory');
-	$actionFactory = App::make('admin_action_factory');
-	$baseUrl = URL::route('admin_dashboard');
-	$route = parse_url($baseUrl);
-
-	//add the view fields
-	$view->config = $config;
-	$view->editFields = $fieldFactory->getEditFields();
-	$view->arrayFields = $fieldFactory->getEditFieldsArrays();
-	$view->actions = $actionFactory->getActionsOptions();
-	$view->baseUrl = $baseUrl;
-	$view->assetUrl = URL::to('packages/atlantis/admin/');
-	$view->route = $route['path'].'/';
-});
-
-
-//header view
-View::composer(array('admin::partials.header'), function($view)
-{
-	$view->menu = App::make('admin_menu')->getMenu();
-	$view->settingsPrefix = App::make('admin_config_factory')->getSettingsPrefix();
-	$view->pagePrefix = App::make('admin_config_factory')->getPagePrefix();
-	$view->configType = App::bound('itemconfig') ? App::make('itemconfig')->getType() : false;
-});
-
-
-
-View::composer(array('admin::layouts.common'), function($view){
-    $locale = Config::get('app.locale');
+View::composer(array('admin::layouts.common','layouts.common'), function($view){
+    $view->appbase = url('/') . '/';
 
     /* Common Assets
     * ---------------------------------------------------------------------*/
@@ -81,40 +15,23 @@ View::composer(array('admin::layouts.common'), function($view){
             $collection->stylesheet('bootstrap-theme.css');
         })->apply('CssMin');
 
-        //$collection->javascript('../components/require.js')->apply('JsMin');
+        $collection->javascript('../components/require.js')->apply('JsMin');
         $collection->javascript('../components/jquery/jquery.js')->apply('JsMin');
         $collection->javascript('../components/jquery/jquery-migrate.js')->apply('JsMin');
         $collection->javascript('../vendor/twitter/bootstrap/dist/js/bootstrap.js')->apply('JsMin');
         $collection->javascript('../components/angularjs/angular.js')->apply('JsMin');
+
+        $collection->directory('packages/atlantis/admin/stylesheet', function($collection){
+            //$collection->stylesheet('css/main.css');
+        })->apply('CssMin');
     });
 
-    //if (!$view->page){
-        Basset::collection('common', function($collection){
-            $collection->directory('packages/atlantis/admin/stylesheet', function($collection){
-                //$collection->stylesheet('css/main.css');
-            })->apply('CssMin');
-        });
 
-        Basset::collection('admin', function($collection){
-            $collection->directory('packages/atlantis/admin/javascript/libs', function($collection){
-                $collection->javascript('knockout/knockout-2.2.0.js');
-                $collection->javascript('knockout/knockout.mapping.js');
-                $collection->javascript('knockout/KnockoutNotification.knockout.min.js');
-                $collection->javascript('knockout/knockout.updateData.js');
-                $collection->javascript('markdown/markdown.js');
-            });
-
-            $collection->directory('packages/atlantis/admin/javascript', function($collection){
-                $collection->javascript('js/knockout.binding.js');
-                $collection->javascript('js/accounting.js');
-                $collection->javascript('libs/history/history.min.js');
-                $collection->javascript('js/admin.js');
-                $collection->javascript('js/settings.js');
-            });
-        });
-    //}
-
+    /* User Assets
+    * ---------------------------------------------------------------------*/
     Basset::collection('user', function($collection){
+        $locale = Config::get('app.locale');
+
         $collection->directory('assets/stylesheet', function($collection)
         {
             $collection->stylesheet('less/user.less')->apply('Less');
@@ -148,8 +65,44 @@ View::composer(array('admin::layouts.common'), function($view){
             $collection->javascript('libs/ckeditor/adapters/jquery.js');
             $collection->javascript('libs/plupload/plupload.full.js');
         })->apply('JsMin');
+
+        $collection->directory('packages/atlantis/admin/javascript/libs', function($collection){
+            $collection->javascript('jquery.noty/jquery.noty.js');
+            $collection->javascript('jquery.noty/layouts/topRight.js');
+            $collection->javascript('jquery.noty/themes/default.js');
+            $collection->javascript('atlantis/plugins/angular.atlantis.js');
+            $collection->javascript('atlantis/atlantis.js');
+            $collection->javascript('atlantis/core/atlantis.alert.js');
+        });
+
+        $collection->directory('assets/javascript/libs', function($collection) use($locale){
+            $collection->javascript('plupload/i18n/'.$locale.'.js');
+            $collection->javascript('jquery.ui/i18n/jquery.ui.datepicker-'.$locale.'.js');
+            $collection->javascript('jquery.ui.timepicker/i18n/jquery-ui-timepicker-'.$locale.'.js');
+            $collection->javascript('jquery.validation/jquery.validationEngine-'.$locale.'.js');
+        });
     });
 
+
+    /* Admin Assets
+    * ---------------------------------------------------------------------*/
+    Basset::collection('admin', function($collection){
+        $collection->directory('packages/atlantis/admin/javascript/libs', function($collection){
+            $collection->javascript('knockout/knockout-2.2.0.js');
+            $collection->javascript('knockout/knockout.mapping.js');
+            $collection->javascript('knockout/KnockoutNotification.knockout.min.js');
+            $collection->javascript('knockout/knockout.updateData.js');
+            $collection->javascript('markdown/markdown.js');
+            $collection->javascript('accounting/accounting.js');
+            $collection->javascript('history/history.min.js');
+        });
+
+        $collection->directory('packages/atlantis/admin/javascript', function($collection){
+            $collection->javascript('js/admin.binding.js');
+            $collection->javascript('js/admin.js');
+            $collection->javascript('js/settings.js');
+        });
+    });
 });
 
 
@@ -169,4 +122,68 @@ View::composer(array('admin::layouts.user'), function($view){
         'applications' => Config::get('admin::admin.sidebar.applications'),
         'user' => Config::get('admin::admin.sidebar.user'),
     )));
+});
+
+
+//header view
+View::composer(array('admin::partials.header'), function($view)
+{
+    $view->menu = App::make('admin_menu')->getMenu();
+    $view->settingsPrefix = App::make('admin_config_factory')->getSettingsPrefix();
+    $view->pagePrefix = App::make('admin_config_factory')->getPagePrefix();
+    $view->configType = App::bound('itemconfig') ? App::make('itemconfig')->getType() : false;
+});
+
+
+//admin index view
+View::composer('admin::admin.admin', function($view)
+{
+    //get a model instance that we'll use for constructing stuff
+    $config = App::make('itemconfig');
+    $fieldFactory = App::make('admin_field_factory');
+    $columnFactory = App::make('admin_column_factory');
+    $actionFactory = App::make('admin_action_factory');
+    $dataTable = App::make('admin_datatable');
+    $model = $config->getDataModel();
+    $baseUrl = URL::route('admin_dashboard');
+    $route = parse_url($baseUrl);
+
+    //add the view fields
+    $view->config = $config;
+    $view->dataTable = $dataTable;
+    $view->primaryKey = $model->getKeyName();
+    $view->editFields = $fieldFactory->getEditFields();
+    $view->arrayFields = $fieldFactory->getEditFieldsArrays();
+    $view->dataModel = $fieldFactory->getDataModel();
+    $view->columnModel = $columnFactory->getColumnOptions();
+    $view->actions = $actionFactory->getActionsOptions();
+    $view->globalActions = $actionFactory->getGlobalActionsOptions();
+    $view->actionPermissions = $actionFactory->getActionPermissions();
+    $view->filters = $fieldFactory->getFiltersArrays();
+    $view->rows = $dataTable->getRows(App::make('db'), $view->filters);
+    $view->formWidth = $config->getOption('form_width');
+    $view->baseUrl = $baseUrl;
+    $view->assetUrl = URL::to('packages/atlantis/admin/');
+    $view->route = $route['path'].'/';
+    $view->itemId = isset($view->itemId) ? $view->itemId : null;
+});
+
+
+//admin settings view
+View::composer('admin::admin.settings', function($view)
+{
+    $config = App::make('itemconfig');
+    $fieldFactory = App::make('admin_field_factory');
+    $actionFactory = App::make('admin_action_factory');
+    $baseUrl = URL::route('admin_dashboard');
+    $route = parse_url($baseUrl);
+
+    //add the view fields
+    $view->config = $config;
+    $view->editFields = $fieldFactory->getEditFields();
+    $view->arrayFields = $fieldFactory->getEditFieldsArrays();
+    $view->actions = $actionFactory->getActionsOptions();
+    $view->baseUrl = $baseUrl;
+    $view->assetUrl = URL::to('packages/atlantis/admin/');
+    $view->route = $route['path'].'/';
 });
