33f8d58538bc057bc24d3b2aa9e83f0c8fb832ee
Azri Jamil <azri@nematix.com>
2014-04-27 17:27:46 +0800

    Moving message translation to Atlantis package

---
+++ src/lang/en/message.php
@@ -0,0 +1,11 @@
+<?php return array(
+    'title' => array(
+        'read_message'  => 'Baca Mesej',
+        'conversations' => 'Komunikasi'
+    ),
+
+    'text' => array(
+        'notification_subject' => 'You have an message from :full_name',
+        'notification_body' => 'Anda telah menerima pesanan dari pengguna sistem ini, sila klik link di bawah untuk membacanya.'
+    )
+);
