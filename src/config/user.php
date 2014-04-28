a038fcac87937b1c867ed299e96729e9ef3d8ff3
Azri Jamil <azri@nematix.com>
2014-04-28 22:42:24 +0800

    Admin user config override

--- src/config/user.php
+++ src/config/user.php
@@ -1,9 +1,12 @@
 <?php return array(
-
     'role_staff_officer' => 'Staff Officer',
 
     'role_staff_manager' => 'Staff Manager',
 
     'role_student' => 'Student',
 
+    'group_view' => array(
+        'staff' => 'Staff',
+        'student' => 'Student'
+    )
 );
