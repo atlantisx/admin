a9be0e0245e1a816bc12262fe180e42cb1e82840
Azri Jamil <azri@nematix.com>
2014-05-26 23:12:38 +0800

    Student & Staff fixed-box layout header logo refactor

--- src/views/user/login.blade.php
+++ src/views/user/login.blade.php
@@ -1,34 +1,30 @@
 @extends('themes/default::layouts.fixed-box')
 
-@section('box')
-    <div class="box">
-        <div class="box-header">
-            <div class="title">{{ trans('admin::user.login_title') }}</div>
-        </div>
+@section('box-header')
+    <div class="title">{{ trans('admin::user.login_title') }}</div>
+@endsection
 
-        <div class="box-content padded">
-            {{ Former::open()->class('separate-sections') }}
-            <div class="input-group addon-left">
-                <span class="input-group-addon" href="#"><i class="fa fa fa-user"></i></span>
-                {{ Former::text('email')->placeholder(trans('admin::user.login_label_login')) }}
-            </div>
-
-            <div class="input-group addon-left">
-                <span class="input-group-addon" href="#"><i class="fa fa fa-key"></i></span>
-                {{ Former::password('password')->placeholder(trans('admin::user.login_label_password')) }}
-            </div>
-
-            <div>
-                <btn id="submit" class="btn btn-blue btn-block">{{ trans('admin::user.login_btn_login') }} <i class="fa fa fa-sign-in"></i></btn>
-            </div>
-            {{ Former::close() }}
-
-            <div>
-                <i class="fa fa fa-umbrella"></i> {{ trans('admin::user.login_text_help', array('link'=>url('public/page/help'))) }}
-            </div>
-        </div>
+@section('box-content')
+    {{ Former::open()->class('separate-sections') }}
+    <div class="input-group addon-left">
+        <span class="input-group-addon" href="#"><i class="fa fa fa-user"></i></span>
+        {{ Former::text('email')->placeholder(trans('admin::user.login_label_login')) }}
     </div>
-@show
+
+    <div class="input-group addon-left">
+        <span class="input-group-addon" href="#"><i class="fa fa fa-key"></i></span>
+        {{ Former::password('password')->placeholder(trans('admin::user.login_label_password')) }}
+    </div>
+
+    <div>
+        <btn id="submit" class="btn btn-blue btn-block">{{ trans('admin::user.login_btn_login') }} <i class="fa fa fa-sign-in"></i></btn>
+    </div>
+    {{ Former::close() }}
+
+    <div>
+        <i class="fa fa fa-umbrella"></i> {{ trans('admin::user.login_text_help', array('link'=>url('public/page/help'))) }}
+    </div>
+@endsection
 
 @section('javascript')
     @parent
