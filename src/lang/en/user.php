5ca27ab7b283aa720b3de93cd61ad7fce07d9c78
Azri Jamil <azri@nematix.com>
2014-07-15 23:19:17 +0800

    Refactor project language file

--- src/lang/en/user.php
+++ src/lang/en/user.php
@@ -1,4 +1,13 @@
 <?php return array(
+    /*
+    |--------------------------------------------------------------------------
+    | User & Auth Language
+    |--------------------------------------------------------------------------
+    |
+    | en - English
+    |
+    */
+
     'menu_label_home'           => 'Home',
 
     'title_register'            => 'User Registration',
@@ -33,24 +42,24 @@
     'btn_draft'                 => 'Save Draft',
     'btn_send'                  => 'Send',
 
-    'registered_title'          => 'Registration sucessful',
-    'registered_text_success'   => 'Your registration is successful, activation email have been sent to <strong>:email</strong> and please click the link provided to activate you account. ' .
-                                   'Please save the activation code below for your future reference if you unable to activate you account through your email.',
-    'registered_label_activation_code' => 'Activation Code',
+    'registered_title'                  => 'Registration successful',
+    'registered_label_activation_code'  => 'Activation Code',
+    'registered_text_success'           => 'Your registration is successful, activation email have been sent to <strong>:email</strong> and please click the link provided to activate you account. ' .
+                                            'Please save the activation code below for your future reference if you unable to activate you account through your email.',
 
-    'activation_title'          => 'Send Activation Link',
-    'activation_btn_send'       => 'Send',
-    'activation_btn_activate'   => 'Activate',
-    'activation_success'        => 'Your activation is successful! Please login to system with credential provided in registration.',
-    'activation_error'          => 'Your activation is unsuccessful, please contact system administration for help.',
-    'activation_resend_success' => 'Your activation code have been successfully send to :email',
-    'activation_email_subject'  => 'Account activation for user :full_name',
-    'activation_email_text'     => 'Please click below link to activate your account',
-    'activation_email_regards'  => 'Thank You,<br> System Administrator',
-    'activation_prompt'         => 'Please enter your login credential to resend activation email',
-    'activate_title'            => 'Manual Activation',
-    'activate_code'             => 'Activation Code',
-    'activate_prompt'           => 'Please enter you activation code to activate an account',
+    'activation_title'                  => 'Send Activation Link',
+    'activation_btn_send'               => 'Send',
+    'activation_btn_activate'           => 'Activate',
+    'activation_success'                => 'Your activation is successful! Please login to system with credential provided in registration.',
+    'activation_error'                  => 'Your activation is unsuccessful, please contact system administration for help.',
+    'activation_resend_success'         => 'Your activation code have been successfully send to :email',
+    'activation_email_subject'          => 'Account activation for user :full_name',
+    'activation_email_text'             => 'Please click below link to activate your account',
+    'activation_email_regards'          => 'Thank You,<br> System Administrator',
+    'activation_prompt'                 => 'Please enter your login credential to resend activation email',
+    'activate_title'                    => 'Manual Activation',
+    'activate_code'                     => 'Activation Code',
+    'activate_prompt'                   => 'Please enter you activation code to activate an account',
 
     'recovery_password_title'           => 'Password Reset',
     'recovery_password_sended'          => 'Password recovery link successfully send to your email, please click provided link to reset your password.',
@@ -63,10 +72,10 @@
     'recovery_password_email_text'      => 'Please click below link to reset your password',
     'recovery_email_regards'            => 'Thank You,<br> System Administrator',
 
-    'login_title'               => 'User Login',
-    'login_label_login'         => 'Username \ email',
-    'login_label_password'      => 'Password',
-    'login_btn_login'           => 'Login',
-    'login_text_register'       => 'Login <a href=":link"><strong>register</strong></a> if you dont have an account',
-    'login_text_help'           => '<a href=":link"><strong>Login help</strong></a>'
+    'login_title'                       => 'User Login',
+    'login_label_login'                 => 'Username \ email',
+    'login_label_password'              => 'Password',
+    'login_btn_login'                   => 'Login',
+    'login_text_register'               => 'Login <a href=":link"><strong>register</strong></a> if you dont have an account',
+    'login_text_help'                   => '<a href=":link"><strong>Login help</strong></a>'
 );
---
+++ src/lang/my/user.php
@@ -0,0 +1,18 @@
+<?php return array(
+    /*
+    |--------------------------------------------------------------------------
+    | User & Auth Language
+    |--------------------------------------------------------------------------
+    |
+    | my - Malaysia
+    |
+    */
+
+    'activation_email_subject'          => 'Pengaktifan Akaun untuk pengguna :full_name',
+    'activation_email_text'             => 'Terima kasih kerana mendaftar dengan Sistem ePendahuluan MARA. Sila klik pada pautan di bawah ini untuk pengaktifan.',
+    'activation_email_regards'          => 'Terima kasih, System Administrator ',
+
+    'recovery_password_email_subject'   => 'Penetapan Katalaluan untuk pengguna :full_name',
+    'recovery_password_email_text'      => 'Katalaluan anda telah berjaya direset. Sila klik pautan di bawah untuk menetapkan katalaluan baru.',
+    'recovery_email_regards'            => 'Terima kasih, System Administrator ',
+);
